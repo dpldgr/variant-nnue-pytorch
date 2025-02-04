af8ea36a3d037a7518a2d3227d211199f893e479
dpldgr <144766863+dpldgr@users.noreply.github.com>
2025-02-04 19:04:46 +0800

    Updated to match data generated.

--- variant.h
+++ variant.h
@@ -1,7 +1,7 @@
 #define FILES 9
 #define RANKS 10
 #define PIECE_TYPES 18
-#define PIECE_TYPES_BITS 5
+#define PIECE_TYPES_BITS 6
 #define PIECE_COUNT 52
 #define POCKETS true
 #define KING_SQUARES 1
