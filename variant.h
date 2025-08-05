81f3d96e953f761282ce0fe880013321e3b0ca4f
dpldgr <144766863+dpldgr@users.noreply.github.com>
2025-08-05 12:12:16 +0800

    Update variant.h

--- variant.h
+++ variant.h
@@ -1,8 +1,7 @@
 #define FILES 9
 #define RANKS 10
-#define PIECE_TYPES 18
-#define PIECE_TYPES_BITS 6
-#define PIECE_COUNT 52
+#define PIECE_TYPES 15
+#define PIECE_COUNT 50
 #define POCKETS true
 #define KING_SQUARES 1
 #define DATA_SIZE 1024
